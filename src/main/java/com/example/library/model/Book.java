def2805905be758157a36ef489243cdf305a713d
Marielaquintanar <marielaqntrr@gmail.com>
2025-04-29 12:32:27 -0600

    archivos pasados

--- src/main/java/com/example/library/model/Book.java
+++ src/main/java/com/example/library/model/Book.java
@@ -1,11 +1,16 @@
 package com.example.library.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import java.sql.Timestamp;
 
+// comentario
 @Entity
 @Table(name = "books") // This specifies the table name in the database
 public class Book {
